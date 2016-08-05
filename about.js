1a1eabf15bbe130c86242da72c822b9a2a88a972
splacorn <curtischong5@gmail.com>
2016-08-05 18:57:03 -0400

    added new homepage naimations
    (dw the old ones are archived)

--- about.js
+++ about.js
@@ -1,25 +1,80 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var sendMail = function (body) {
         window.location.href = "mailto:info@changethroughcode.com?body=" + body;
-    }; 
-   document.getElementById('questionIn').onkeypress = function (e) {
+    };
+
+    var indexOfMax = function (arr) {
+        if (arr.length === 0) {
+            return -1;
+        }
+
+        var max = arr[0];
+        var maxIndex = 0;
+
+        for (var b = 0; b < arr.length; b++) {
+            console.log("count"+b)
+            console.log(arr[b]);
+            console.log(max);
+            if (arr[b] > max) {
+                max = arr[b];
+                maxIndex = b;
+                console.log("hi  " + b);
+            }
+            console.log("m"+max)
+            console.log("i"+maxIndex)
+        }
+        return maxIndex;
+    };
+    var provideAns = function (ans) {
+        $("#incorrectAnswer").fadeOut(400);
+        $("#questionAnswer").fadeOut(400, function () {
+            $("#questionAnswer").html(ans);
+            $("#questionAnswer").fadeIn(800, function () {
+                $("#incorrectAnswer").animate({
+                    marginTop: 40
+                }, {
+                    queue: false,
+                    duration: 800
+                });
+                $("#incorrectAnswer").html("Didn't really answer your question? Send us an email here.");
+                $("#incorrectAnswer").fadeIn(1000);
+            });
+        });
+    };
+    //Change Through Code was formed in June 2016 as a response to engage programmers into communal problems."
+
+    var answersArray = ["Like many regions, Ontario is burdenned and taxed with problems that affects those who calls it home (if you can even get home from the traffic). And an inspiration came after we realized that people who virtually marries their computer all day, has the potential to look at the community in a broader scalle and make a lasting difference.", "Change through Code was formed in July 2016", "Change through Code was CoFounded by Curtis Chong and Jake Xia."];
+    //0 = why change through code was formed
+    //1 = when was it formed
+    //2 = who formed it
+
+
+    document.getElementById('questionIn').onkeypress = function (e) {
         if (!e) e = window.event;
         var keyCode = e.keyCode || e.which;
         if (keyCode == '13') {
+
+            var answerArray = [0, 0, 0];
+
             var question = $("#questionIn").val().toLowerCase();
+            if (question.indexOf("why") != -1) {
+                answerArray[0]++;
+            }
+            if (question.indexOf("form") != -1) {
+                answerArray[0]++;
+                answerArray[1]++;
+                answerArray[2]++;
+            }
             if (question.indexOf("when") != -1) {
-                $("#questionAnswer").html("Change Through Code was formed in June 2016...");
-                $("#questionAnswer").fadeIn(800, function () {
-                    $("#incorrectAnswer").animate({
-                        marginTop: 40
-                    }, {
-                        queue: false,
-                        duration: 800
-                    });
-                    $("#incorrectAnswer").html("Didn't really answer your question? Send us an email here.");
-                    $("#incorrectAnswer").fadeIn(1000);
-                });
-            } else {
+                answerArray[1]++;
+            }
+            if (question.indexOf("who") != -1) {
+                answerArray[2]++;
+            }
+            console.log(answerArray);
+            var sortedArray = answerArray;
+            sortedArray.sort();
+            if (sortedArray[2] == 0) {
                 $("#questionAnswer").html("It seems like we can't find an answer to your question.");
                 $("#questionAnswer").fadeIn(800, function () {
                     $("#incorrectAnswer").animate({
@@ -31,9 +86,16 @@ $(document).ready(function(){
                     $("#incorrectAnswer").html("But send us an email here if you want to know more.");
                     $("#incorrectAnswer").fadeIn(1000);
                 });
+            } else {
+                //console.log(indexOfMax(answerArray));
+                provideAns(answersArray[indexOfMax(answerArray)]);
             }
         }
     }
+
+
+
+
     $("#incorrectAnswer").on("click", function () {
         sendMail($("#questionIn").val());
     });
--- main.js
+++ main.js
@@ -2,6 +2,137 @@ $(document).ready(function () {
     $("#mainImage").css("height", screen.height - 101);
 
 
+
+    //this will start the starup animation
+
+    //changethrough code words
+
+
+
+
+
+
+    //change through code statement
+    /*var titleShowCount = 0;
+    var titleShow = setInterval(function(){
+        titleShowCount++;
+      $("#t"+titleShowCount).fadeTo(1,200);
+      $("#t10").fadeTo(1,200);
+    },70);*/
+
+
+
+    /*
+    $("#s1").animate({top: 200,queue: false},2400);
+    $("#s2").fadeIn(1000);
+    $("#s3").animate({top: 130,queue: false},2400,function(){
+        $("#s4").fadeIn(2000);
+    });
+    $("#s5").animate({top: -5,queue: false},2400);
+    setTimeout(function(){
+        $("#s6").animate({top: 75,queue: false},2400);
+    },400);
+    $("#s7").animate({top: 70,queue: false},2400);
+    $("#s8").animate({top: 5,queue: false},2400);
+    $("#s9").animate({top: 10,queue: false},2400);
+    $("#s10").animate({top: 65,queue: false},2400);
+    $("#s11").animate({top: 0,queue: false},2400);
+    $("#s12").animate({top: 60,queue: false},2400);*/
+
+
+
+
+
+    /*
+    
+    
+    //animate email link as it comes into view
+    function onVisibilityChange(el, callback) {
+    var old_visible;
+    return function () {
+        var visible = isElementInViewport(el);
+        if (visible != old_visible) {
+            old_visible = visible;
+            if (typeof callback == 'function') {
+                callback();
+            }
+        }
+    }
+}
+
+var handler = onVisibilityChange($("#emailform"), function() {
+    $("#emailform").animate({width: 400,queue: false},2400);
+});
+
+
+//jQuery
+$(window).on('DOMContentLoaded load resize scroll', handler); 
+    
+    
+    */
+
+    $("#homepageFront").css("height", window.innerHeight);
+    $("#logoIcon").fadeIn(1600);
+    $("#logoIcon").css("top", (window.innerHeight / 2) - 100);
+    $("#logoIcon").css("left", (window.innerWidth / 2) - 50);
+    $("#logoText").css("top", ((window.innerHeight / 2) - 55));
+    //$("#logoText").css("top", ((window.innerHeight/2)-50));
+    setTimeout(function () {
+        $("#logoIcon").animate({
+            left: window.innerWidth / 2 - 200
+        }, {
+            queue: false
+        });
+
+        $("#logoText").animate({
+            right: -10,
+            queue: false
+        }, {
+            queue: false
+        });
+
+        $("#t1").fadeTo(1900, 1);
+        $("#t2").fadeTo(660, 1);
+        $("#t3").fadeTo(500, 1);
+        $("#t4").fadeTo(800, 1);
+        $("#t5").fadeTo(1700, 1);
+        $("#t6").fadeTo(600, 1);
+        $("#t7").fadeTo(800, 1);
+        $("#t8").fadeTo(1300, 1);
+        $("#t9").fadeTo(800, 1);
+        $("#t10").fadeTo(2000, 1);
+        $("#t11").fadeTo(950, 1);
+        $("#t12").fadeTo(700, 1);
+        $("#t13").fadeTo(2900, 1);
+        $("#t14").fadeTo(900, 1);
+        $("#t15").animate({
+            top: 500,
+            queue: false
+        }, 1200);
+        $("#t15").fadeTo(1300, 1);
+        $("#t16").fadeTo(1200, 1);
+        $("#t17").fadeTo(800, 1);
+
+
+        setTimeout(function () {
+
+            /*$("#logoText").animate({
+                top: $("#logoText").offset().top-100
+            }, {
+                queue: false
+            }, 1800);
+
+
+            $("#logoIcon").animate({
+                top: $("#logoIcon").offset().top-100
+            }, {
+                queue: false
+            }, 1800);*/
+            $("#logoIcon").fadeOut(2500);
+        }, 1200);
+
+    }, 2000);
+
     setInterval(function () {
 
 
@@ -9,9 +140,9 @@ $(document).ready(function () {
             alert("true");
         }
     }, 300);
-var sendMail = function (body) {
+    var sendMail = function (body) {
         window.location.href = "mailto:info@changethroughcode.com?body=" + body;
-    }; 
+    };
 
 
 
@@ -30,7 +161,7 @@ var sendMail = function (body) {
             }
         });
     });
-    
+
     document.getElementById('emailform').onkeypress = function (e) {
         if (!e) e = window.event;
         var keyCode = e.keyCode || e.which;
